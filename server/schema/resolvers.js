d5307d2ef3b5c342ec833a09648f3219a4d9e9e7
AKen00 <ajken04@gmail.com>
2021-01-14 10:44:11 -0500

    React routes added

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -2,7 +2,6 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require('../models');
 const { signToken } = require('../utils/auth');
 
-
 const resolvers = {
     Query: {
       me: async (parent, args, context) => {
